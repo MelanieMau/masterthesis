cf8e7c9ada63c0ebe9972ecb8946144a830cc06c
MelanieMau <melanie.mau@mailbox.tu-berlin.de>
2018-11-17 23:25:24 +0100

    Add files via upload

--- mood2Sheets/app/index.js
+++ mood2Sheets/app/index.js
@@ -2,18 +2,28 @@ import document from "document";
 import * as messaging from "messaging";
 
 const btn_happy = document.getElementById("btn_happy");
+const btn_bad = document.getElementById("btn-bad");
 const eventName = "mood2sheet";
 
-
-btn_happy.addEventListener("Happy", () => {
-  sendEventIfReady(eventName);
+if (btn_happy){
+  btn_happy.addEventListener("click", () => {
+    sendEventIfReady(eventName, "good");
+  });
+}
+if (btn_bad){
+btn_bad.addEventListener("click", () => {
+  sendEventIfReady(eventName, "bad");
 });
+}
 
-function sendEventIfReady(eventName) {
+function sendEventIfReady(eventName, val) {
   if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
-    messaging.peerSocket.send({eventName: eventName});
+    messaging.peerSocket.send({eventName: eventName, result: val});
   }
 }
+
+
+
 
 
 
